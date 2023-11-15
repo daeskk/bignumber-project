453db0b616add9f9c910492e8ca1cb2124b7e9a3
Isabele Cristina Felix Santos <isabelebrasil4@hotmail.com>
2023-11-15 19:02:37 -0300

    correct add + part implementation of subtract

--- expression.c
+++ expression.c
@@ -30,12 +30,43 @@ static struct big_number *add_positive_big_numbers(struct big_number *bg1, struc
     return create_big_number(result, i, false);
 }
 
-// Subtração - falta implementar
+// Processo de subtração de números positivos
+static struct big_number *subtract_positive_big_numbers(struct big_number *bg1, struct big_number *bg2) {
+    int max_length = bg1->length > bg2->length ? bg1->length : bg2->length;
+    char *result = calloc(max_length + 1, sizeof(char));
+
+    int rest = 0;
+    int i = 0;
+
+    for (; i < max_length; i++) {
+        int digit1 = i < bg1->length ? bg1->digits[bg1->length - i - 1] - '0' : 0;
+        int digit2 = i < bg2->length ? bg2->digits[bg2->length - i - 1] - '0' : 0;
+
+        int sub = digit1 - digit2 - rest;
+        result[i] = (sub % 10) + '0';
+        rest = sub / 10;
+    }
+
+    if (rest > 0) {
+        result[i++] = rest + '0';
+    }
+
+    reverse_string(result);
+    
+    return create_big_number(result, i, false);
+}
+
+
+// Subtração - falta implementar sinais diferentes
 static struct big_number *subtract(struct big_number *bg1, struct big_number *bg2) {
     struct big_number *result = NULL;
-
+    if (bg1->negative == bg1->negative) {
+        result = subtract_positive_big_numbers(bg1, bg2);
+        result->negative = bg1->negative;
+    }
     return result;
 }
+
 
 // Multiplicação - falta implementar
 static struct big_number *multiply(struct big_number *bg1, struct big_number *bg2) {
@@ -45,31 +76,32 @@ static struct big_number *multiply(struct big_number *bg1, struct big_number *bg
 }
 
 // Adição - caso seja entre dois números negativos, trata como positivos, para sinais diferentes subtrai o menor do maior
-// Obs: verificar se mantém sinal correto após o cálculo
 static struct big_number *add(struct big_number *bg1, struct big_number *bg2) {
     struct big_number *result = NULL;
-
-
     if (bg1->negative == bg1->negative) {
         result = add_positive_big_numbers(bg1, bg2);
+        result->negative = bg1->negative;
     } else {
         if (bg1->length > bg2->length) {
             result = subtract(bg1, bg2);
+            result->negative = bg1->negative;
         } else if (bg1->length < bg2->length) {
             result = subtract(bg2, bg1);
+            result->negative = bg2->negative;
         } else {
             for (int i = 0; i < bg1->length; i++) {
                 if (bg1->digits[i] > bg2->digits[i]) {
                     result = subtract(bg1, bg2);
+                    result->negative = bg1->negative;
                     break;
                 } else if (bg1->digits[i] < bg2->digits[i]) {
                     result = subtract(bg2, bg1);
+                    result->negative = bg2->negative;
                     break;
                 }
             }
         }
     }
-
     return result;
 }
 
