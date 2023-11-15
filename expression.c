8124db8cf2203265206c385ff20fdac3afb13c23
Isabele Cristina Felix Santos <isabelebrasil4@hotmail.com>
2023-11-15 20:50:00 -0300

    add and subtract different signs

--- expression.c
+++ expression.c
@@ -32,7 +32,7 @@ static struct big_number *add_positive_big_numbers(struct big_number *bg1, struc
 
 // Processo de subtração de números positivos
 static struct big_number *subtract_positive_big_numbers(struct big_number *bg1, struct big_number *bg2) {
-    int max_length = bg1->length > bg2->length ? bg1->length : bg2->length;
+    int max_length = bg1->length;
     char *result = calloc(max_length + 1, sizeof(char));
 
     int rest = 0;
@@ -42,9 +42,14 @@ static struct big_number *subtract_positive_big_numbers(struct big_number *bg1, 
         int digit1 = i < bg1->length ? bg1->digits[bg1->length - i - 1] - '0' : 0;
         int digit2 = i < bg2->length ? bg2->digits[bg2->length - i - 1] - '0' : 0;
 
-        int sub = digit1 - digit2 - rest;
-        result[i] = (sub % 10) + '0';
-        rest = sub / 10;
+        if (digit1 < digit2 + rest) {
+            digit1 = digit1 + 10;
+            result[i] = (digit1 - digit2 - rest) + '0';
+            rest = 1;
+        } else {
+            result[i] = (digit1 - digit2 - rest) + '0';
+            rest = 0;
+        }
     }
 
     if (rest > 0) {
@@ -52,16 +57,24 @@ static struct big_number *subtract_positive_big_numbers(struct big_number *bg1, 
     }
 
     reverse_string(result);
-    
+
     return create_big_number(result, i, false);
 }
 
 
-// Subtração - falta implementar sinais diferentes
+// Subtração - sinais iguais subtrai, sinais diferentes soma
 static struct big_number *subtract(struct big_number *bg1, struct big_number *bg2) {
     struct big_number *result = NULL;
-    if (bg1->negative == bg1->negative) {
-        result = subtract_positive_big_numbers(bg1, bg2);
+    if (bg1->negative == bg2->negative) {
+        if (bg1->length > bg2->length || (bg1->length == bg2->length && strcmp(bg1->digits, bg2->digits) >= 0)) {
+            result = subtract_positive_big_numbers(bg1, bg2);
+            result->negative = bg1->negative;
+        } else {
+            result = subtract_positive_big_numbers(bg2, bg1);
+            result->negative = !bg1->negative;
+        }
+    } else {
+        result = add_positive_big_numbers(bg1, bg2);
         result->negative = bg1->negative;
     }
     return result;
@@ -75,19 +88,19 @@ static struct big_number *multiply(struct big_number *bg1, struct big_number *bg
     return result;
 }
 
-// Adição - caso seja entre dois números negativos, trata como positivos, para sinais diferentes subtrai o menor do maior
+// Subtração - sinais iguais soma, sinais diferentes subtrai
 static struct big_number *add(struct big_number *bg1, struct big_number *bg2) {
     struct big_number *result = NULL;
-    if (bg1->negative == bg1->negative) {
+    if (bg1->negative == bg2->negative) {
         result = add_positive_big_numbers(bg1, bg2);
         result->negative = bg1->negative;
     } else {
-        if (bg1->length > bg2->length) {
-            result = subtract(bg1, bg2);
+        if (bg1->length > bg2->length || (bg1->length == bg2->length && strcmp(bg1->digits, bg2->digits) >= 0)) {
+            result = subtract_positive_big_numbers(bg1, bg2);
             result->negative = bg1->negative;
-        } else if (bg1->length < bg2->length) {
-            result = subtract(bg2, bg1);
-            result->negative = bg2->negative;
+        } else {
+            result = subtract_positive_big_numbers(bg2, bg1);
+            result->negative = !bg1->negative;
         }
     }
     return result;
